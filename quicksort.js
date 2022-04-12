8c7e628900744a4309e2295ab800e4a43498f2b4
n00bNn00b <reldewan9@gmail.com>
2022-04-12 18:33:27 +0600

    Quick Sort Algorithm in JavaScript. read the comments in the code to understand

---
+++ quicksort.js
@@ -0,0 +1,31 @@
+// quicksort solution
+
+const quickSort = (array) => {
+  if (array.length === 1) {
+    return array;
+  }
+
+  const pivot = array[array.length - 1]; //pivot array
+  const leftArr = [];
+  const rightArr = [];
+
+  // here I am using es6 for loop instead of traditional for loop and ternary operator. You may use traditional for loop if you're comfortable with it. If needed also the traditional for loop, I may add later. I have used spread operator too instead of using array.push(something) to reduce code.
+  for (const element of array.slice(0, array.length - 1)) {
+    element < pivot ? leftArr.push(element) : rightArr.push(element);
+  }
+  // recursion
+  if (leftArr.length > 0 && rightArr.length > 0) {
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  } else if (leftArr.length > 0) {
+    return [...quickSort(leftArr), pivot];
+  } else {
+    return [pivot, ...quickSort(rightArr)];
+  }
+};
+
+const givenArray = [12, 11, 13, 5, 6, 7, 100, 50, 1000, 1, 3, 2];
+
+// to understand I am printing the given array. you may remove this line
+console.log("Given Array: ", givenArray);
+// solution array of quicksort
+console.log("Quick Sort: ", quickSort(givenArray));
